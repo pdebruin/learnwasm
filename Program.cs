4d9d66dc5814318152e5ec7f343f3610fa2cbf68
Pieter de Bruin <pdebruin@users.noreply.github.com>
2026-01-22 13:12:52 +0000

    learn mcp client

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using learnwasm.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
